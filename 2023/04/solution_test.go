bde3b7a997e9ccf309c1c122f2a888b988b34aa4
monicagangwar <monicagangwar10@gmail.com>
2024-11-19 19:06:26 +0100

    adding day 4 part 2

--- 2023/04/solution_test.go
+++ 2023/04/solution_test.go
@@ -16,7 +16,7 @@ func TestSolution(t *testing.T) {
 		name            string
 		input           string
 		expectedPartOne int
-		expectedPartTwo int64
+		expectedPartTwo int
 	}
 
 	tests := []test{
@@ -24,12 +24,12 @@ func TestSolution(t *testing.T) {
 			name:            "with sample",
 			input:           "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
 			expectedPartOne: 13,
-			expectedPartTwo: -1,
+			expectedPartTwo: 30,
 		}, {
 			name:            "with large input",
 			input:           input,
 			expectedPartOne: ***REMOVED***,
-			expectedPartTwo: -1,
+			expectedPartTwo: 0,
 		},
 	}
 
@@ -120,6 +120,26 @@ func partOne(cards []Card) int {
 	return totalPoints
 }
 
-func partTwo(cards []Card) int64 {
-	return -1
+func partTwo(cards []Card) int {
+	cardCount := make(map[int]int, len(cards))
+
+	for _, card := range cards {
+		cardCount[card.ID] = 1
+	}
+
+	for _, card := range cards {
+		curCardCount := cardCount[card.ID]
+		matchCount := card.getMatchCount()
+		for i := card.ID + 1; i <= card.ID+matchCount; i++ {
+			cardCount[i] += curCardCount
+		}
+	}
+
+	totalCards := 0
+
+	for _, count := range cardCount {
+		totalCards += count
+	}
+
+	return totalCards
 }
