5d6a673ca258a56548a1d43b0f10779ea5d64fda
monicagangwar <monicagangwar10@gmail.com>
2025-12-04 12:07:14 +0100

    aoc 2025 day 4

---
+++ 2025/04/solution_test.go
@@ -0,0 +1,123 @@
+package template
+
+import (
+	_ "embed"
+	"strings"
+	"testing"
+)
+
+//go:embed input.txt
+var input string
+
+//go:embed sample.txt
+var sample string
+
+func TestSolution(t *testing.T) {
+	type test struct {
+		name            string
+		input           string
+		expectedPartOne int
+		expectedPartTwo int
+	}
+
+	tests := []test{
+		{
+			name:            "with sample",
+			input:           sample,
+			expectedPartOne: 13,
+			expectedPartTwo: 43,
+		}, {
+			name:            "with large input",
+			input:           input,
+			expectedPartOne: 0,
+			expectedPartTwo: 0,
+		},
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+			grid := parseInput(tst.input)
+			if tst.expectedPartOne != -1 {
+				if got := partOne(grid); got != tst.expectedPartOne {
+					t.Errorf("partOne() = %v, want %v", got, tst.expectedPartOne)
+				}
+			}
+
+			if tst.expectedPartTwo != -1 {
+				if got := partTwo(grid); got != tst.expectedPartTwo {
+					t.Errorf("partTwo() = %v, want %v", got, tst.expectedPartTwo)
+				}
+			}
+		})
+
+	}
+
+}
+
+func parseInput(input string) [][]bool {
+	lines := strings.Split(input, "\n")
+	grid := make([][]bool, len(lines))
+	for i, line := range lines {
+		grid[i] = make([]bool, len(line))
+		for j, r := range line {
+			if r == '@' {
+				grid[i][j] = true
+			} else {
+				grid[i][j] = false
+			}
+		}
+	}
+	return grid
+}
+
+func partOne(grid [][]bool) int {
+	rollsCanBeAccessed := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if canBeAccessed(grid, i, j) {
+				rollsCanBeAccessed++
+			}
+		}
+	}
+	return rollsCanBeAccessed
+}
+
+func partTwo(grid [][]bool) int {
+	rollsCanBeAccessed := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			if canBeAccessed(grid, i, j) {
+				rollsCanBeAccessed++
+				grid[i][j] = false
+				i = 0
+				j = 0
+			}
+		}
+	}
+	return rollsCanBeAccessed
+}
+
+func canBeAccessed(grid [][]bool, i, j int) bool {
+	if !grid[i][j] {
+		return false
+	}
+	neighbors := [8][2]int{
+		[2]int{i, j - 1},
+		[2]int{i, j + 1},
+		[2]int{i - 1, j - 1},
+		[2]int{i - 1, j},
+		[2]int{i - 1, j + 1},
+		[2]int{i + 1, j - 1},
+		[2]int{i + 1, j},
+		[2]int{i + 1, j + 1},
+	}
+	countNeighbours := 0
+	lenGridX := len(grid[i])
+	lenGridY := len(grid)
+	for _, n := range neighbors {
+		if n[0] >= 0 && n[0] < lenGridX && n[1] >= 0 && n[1] < lenGridY && grid[n[0]][n[1]] {
+			countNeighbours++
+		}
+	}
+	return countNeighbours < 4
+}
